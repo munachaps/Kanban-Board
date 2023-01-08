e1a5872348f411d808ae4eece250d1ca54b92409
munachaps <munachaps@gmail.com>
2023-01-08 23:39:04 +0200

    BoardController Changes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BoardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,15 +20,15 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 //  Board API Routes
-Route::get('boards/{user_id}', 'BoardController@index');
-Route::get('board/{id}/{user_id}', 'BoardController@show');
-Route::post('board/{user_id}', 'BoardController@store');
-Route::put('board/{user_id}', 'BoardController@update');
-Route::delete('board/{id}/{user_id}', 'BoardController@destroy');
+Route::get('boards/{user_id}', 'App\Http\Controllers\BoardController@index');
+Route::get('board/{id}/{user_id}', 'App\Http\Controllers\BoardController@show');
+Route::post('board/{user_id}', 'App\Http\Controllers\BoardController@store');
+Route::put('board/{user_id}', 'App\Http\Controllers\BoardController@update');
+Route::delete('board/{id}/{user_id}', 'App\Http\Controllers\BoardController@destroy');
 
 //  Task API Routes
-Route::get('tasks/{user_id}', 'TaskController@index');
-Route::get('task/{id}/{user_id}', 'TaskController@show');
-Route::post('task/{user_id}', 'TaskController@store');
-Route::put('task/{user_id}', 'TaskController@update');
-Route::delete('task/{id}/{user_id}', 'TaskController@destroy');
+Route::get('tasks/{user_id}', 'App\Http\Controllers\TaskController@index');
+Route::get('task/{id}/{user_id}', 'App\Http\Controllers\TaskController@show');
+Route::post('task/{user_id}', 'App\Http\Controllers\TaskController@store');
+Route::put('task/{user_id}', 'App\Http\Controllers\TaskController@update');
+Route::delete('task/{id}/{user_id}', 'App\Http\Controllers\TaskController@destroy');
